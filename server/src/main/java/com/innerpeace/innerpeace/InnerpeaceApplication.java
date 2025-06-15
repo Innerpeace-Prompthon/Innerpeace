e07ba3a56564cd4f2103555191f40350d490563c
Sangwonee <111265742+Sangwonee@users.noreply.github.com>
2025-06-15 22:38:12 +0900

    Initialize project

---
+++ server/src/main/java/com/innerpeace/innerpeace/InnerpeaceApplication.java
@@ -0,0 +1,13 @@
+package com.innerpeace.innerpeace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerpeaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerpeaceApplication.class, args);
+    }
+
+}
