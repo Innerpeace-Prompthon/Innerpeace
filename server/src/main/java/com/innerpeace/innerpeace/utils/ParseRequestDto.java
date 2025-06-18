8a25e1fdee27806b7b0926ca892402ee4e445cef
TaeJeong-Eom <taejeong0416@gmail.com>
2025-06-18 19:22:03 +0900

    feat: Response Parsing

--- server/src/main/java/com/innerpeace/innerpeace/utils/ParseRequestDto.java
+++ server/src/main/java/com/innerpeace/innerpeace/utils/ParseRequestDto.java
@@ -1,22 +1,8 @@
 package com.innerpeace.innerpeace.utils;
 
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-/**
- * 파싱 요청 DTO
- * 클라이언트에서 파싱할 텍스트를 전달할 때 사용
- */
 @Data
-@NoArgsConstructor
 public class ParseRequestDto {
-
-    /**
-     * 파싱할 LaaS 응답 텍스트
-     */
-    private String text;
-
-    public ParseRequestDto(String text) {
-        this.text = text;
-    }
+    private String message;
 }
--- server/src/main/java/com/innerpeace/innerpeace/utils/ResponseParserService.java
+++ server/src/main/java/com/innerpeace/innerpeace/utils/ResponseParserService.java
@@ -2,26 +2,8 @@ package com.innerpeace.innerpeace.utils;
 
 import com.innerpeace.innerpeace.utils.ParseRequestDto;
 import com.innerpeace.innerpeace.utils.ParseResponseDto;
-import com.innerpeace.innerpeace.utils.RawJsonResponseDto;
 
-/**
- * LaaS 응답 파싱 서비스 인터페이스
- */
 public interface ResponseParserService {
-
-    /**
-     * LaaS 응답 텍스트를 구조화된 장소 정보로 파싱
-     *
-     * @param request 파싱 요청 DTO
-     * @return 파싱된 장소 정보 응답 DTO
-     */
-    ParseResponseDto parseToPlaces(ParseRequestDto request);
-
-    /**
-     * LaaS 응답 텍스트를 Raw JSON 문자열로 파싱
-     *
-     * @param request 파싱 요청 DTO
-     * @return Raw JSON 응답 DTO
-     */
-    RawJsonResponseDto parseToJson(ParseRequestDto request);
+    ParseResponseDto parse(ParseRequestDto request);
 }
+
