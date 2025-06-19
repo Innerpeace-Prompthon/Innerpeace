0424a1052e525948846676b98d656779dc155414
kmuseong <antjd0419@gmail.com>
2025-06-20 01:19:24 +0900

    fix : remove-merge-markers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(<App />);
-=======
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
->>>>>>> 09b69170f3912044cf962b250774eab209ac6a1b
