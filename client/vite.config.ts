6311082023d60dc2609bf0321ac066eef5dea13b
kmuseong <antjd0419@gmail.com>
2025-06-19 19:39:14 +0900

    fix : remove merge conflict markers

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
-=======
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import path from "path"
-
->>>>>>> 09b69170f3912044cf962b250774eab209ac6a1b
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-<<<<<<< HEAD
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
-=======
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
->>>>>>> 09b69170f3912044cf962b250774eab209ac6a1b
+});
