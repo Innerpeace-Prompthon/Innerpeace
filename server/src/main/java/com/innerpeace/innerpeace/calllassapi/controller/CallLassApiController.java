27fbe88303050696c6c6e30df4709757118fb727
Sangwonee <sangw12477@gmail.com>
2025-06-24 20:26:18 +0900

    fix: rename endpoint path, and refactor ResponseDto structure

--- server/src/main/java/com/innerpeace/innerpeace/calllassapi/controller/CallLassApiController.java
+++ server/src/main/java/com/innerpeace/innerpeace/calllassapi/controller/CallLassApiController.java
@@ -1,21 +1,25 @@
 package com.innerpeace.innerpeace.calllassapi.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.innerpeace.innerpeace.calllassapi.dto.CallLassApiRequestDto;
-import com.innerpeace.innerpeace.calllassapi.dto.CallLassApiResponseDto;
+import com.innerpeace.innerpeace.calllassapi.dto.ResponseDto;
 import com.innerpeace.innerpeace.calllassapi.service.CallLassApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/lass")
-public class CallLassApiController {
+@RequestMapping("/api/laas")
+public class CallLassApiController  {
 
     private final CallLassApiService callLassApiService;
 
     @PostMapping("/chat")
-    public ResponseEntity<CallLassApiResponseDto> callLassApi(@RequestBody CallLassApiRequestDto requestDto) {
+    public ResponseEntity<ResponseDto> callLassApi(@RequestBody CallLassApiRequestDto requestDto) throws JsonProcessingException {
 
         return ResponseEntity.ok(callLassApiService.requestChatCompletion(requestDto));
     }
